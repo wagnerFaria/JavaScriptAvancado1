c32735696e18a31dd5326aead6547da0bb80b4e8
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2019-04-16 16:30:58 -0400

    Aula_02-08--Programacao Defensiva

--- aluraframe/client/js/app/models/Negociacao.js
+++ aluraframe/client/js/app/models/Negociacao.js
@@ -1,9 +1,10 @@
 class Negociacao {
 
     constructor(data = new Date(), quantidade = 1, valor = 0) {
-        this._data = data;
+        this._data = new Date(data.getTime());
         this._quantidade = quantidade;
         this._valor = valor;
+        Object.freeze(this);
     }
 
     get volume() {
@@ -11,7 +12,7 @@ class Negociacao {
     }
 
     get data() {
-        return this._data;
+        return new Date(this._data.getTime());
     }
 
     get quantidade() {
