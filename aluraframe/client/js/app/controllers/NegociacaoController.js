4b9d9e8690a7592f9fc5882c94ec42d0fce2231a
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2019-04-25 17:14:19 -0400

    Aula_4-06

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,21 +6,30 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
 
         event.preventDefault();
 
-        let helper = new DateHelper();
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacoes.negociacoes);
+    }
 
-        let negociacao = new Negociacao(
-            helper.textoParaData(this._inputData.value),
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
+    }
 
-        console.log(negociacao);
-        console.log(helper.dataParaTexto(negociacao.data));
+    _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+        this._inputData.focus();
     }
 }
--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,14 +1,14 @@
 class DateHelper {
 
-    dataParaTexto(data) {
-        return data.getDate()
-            + '/'
-            + (data.getMonth() + 1)
-            + '/'
-            + data.getFullYear();
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+    static dataParaTexto(data) {
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
 
-    textoParaData(texto) {
+    static textoParaData(texto) {
+        if (!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('Deve estar no formado aaaa-mm-dd');
         return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
     }
 
