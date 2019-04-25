c6619526d87bb8185d9ad512ef4414e5034f2b9e
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2019-04-25 15:09:43 -0400

    Aula_04-01

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,25 +1,29 @@
-
-
 class NegociacaoController {
-
+    
     constructor() {
+        
         let $ = document.querySelector.bind(document);
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
     }
-
+    
     adiciona(event) {
+        
         event.preventDefault();
-
-
-        console.log(this._inputData.value);
-        console.log(this._inputQuantidade.value);
-        console.log(this._inputValor.value);
-        let data = new Date(this._inputData.value.split('-'));
-        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
-
-        console.log('Negociação: ', negociacao);
-        //adicionar negociacao a uma lista  
+      
+        let data = new Date(
+            ...this._inputData.value
+                .split('-')
+                .map((item, indice) => item - indice % 2)
+        );
+            
+        let negociacao = new Negociacao(
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value  
+        );
+        
+        console.log(negociacao);
     }
 }
