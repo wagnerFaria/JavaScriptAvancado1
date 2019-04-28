f4ffd32a4e7909950c3f78c47890adb19856c2b5
Wagner Faria do Amaral <wagnerfaria1601@gmail.com>
2019-04-28 10:13:32 -0400

    Aula_06-04

--- aluraframe/client/js/app/views/NegociacoesView.js
+++ aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,12 +1,11 @@
-class NegociacoesView {
-    
+class NegociacoesView extends View {
+
     constructor(elemento) {
-        
-        this._elemento = elemento;
+        super(elemento);
     }
-    
-    _template(model) {
-        
+
+    template(model) {
+
         return `
         <table class="table table-hover table-bordered">
             <thead>
@@ -40,10 +39,5 @@ class NegociacoesView {
             
         </table>
         `;
-    }
-    
-    update(model) {
-        
-        this._elemento.innerHTML = this._template(model);
     }
 }
