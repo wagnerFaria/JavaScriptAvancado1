12649dbad3344ceebce04a56a6ab92c159d6a61c
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2019-05-06 13:58:38 -0400

    Parte-2__Aula_02-22

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,6 +6,20 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
+        let self = this;
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver) {
+                if (['adiciona', 'esvazia'].includes(prop) && typeof (target[prop]) == typeof (Function)) {
+                    return function () {
+                        console.log(`Interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+                return Reflect.get(target, prop, receiver);
+            }
+        });
         //????????
         // this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
 
