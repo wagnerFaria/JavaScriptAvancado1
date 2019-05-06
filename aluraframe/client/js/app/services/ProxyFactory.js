2693c8f552c47c8d5b8fb7729f3509b854415139
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2019-05-06 16:35:19 -0400

    Parte-2__Aula_03-11

--- aluraframe/client/js/app/services/ProxyFactory.js
+++ aluraframe/client/js/app/services/ProxyFactory.js
@@ -1,30 +1,39 @@
 class ProxyFactory {
 
     static create(objeto, props, acao) {
-        return new Proxy(objeto, {
-            get(target, prop, receiver) {
-                if (props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
-                    return function () {
-                        console.log(`Interceptando ${prop}`);
-                        Reflect.apply(target[prop], target, arguments);
-                        return acao(target);
-                    }
-                }
-                return Reflect.get(target, prop, receiver);
-            },
-            set(target, prop, value, receiver) {
-                console.log(`target: ${prop} \nprop: ${prop} \nvalue: ${value} \nreceiver: ${receiver}`);
-                if (props.includes(prop)) {
-                    target[prop] = value;
-                    acao(target);
-                }
 
-                return Reflect.set(target, prop, value, receiver);
-            }
+        return new Proxy(objeto, {
+
+                get(target, prop, receiver) {
+
+                    if(props.includes(prop) && ProxyFactory._ehFuncao(target[prop])) {
+
+                        return function() {
+                    
+                            console.log(`interceptando ${prop}`);
+                            let retorno = Reflect.apply(target[prop], target, arguments);
+                            acao(target);
+                            return retorno;
+                        }
+                    }
+
+                    return Reflect.get(target, prop, receiver);
+                },
+
+                set(target, prop, value, receiver) {
+
+                    if(props.includes(prop)) {
+                        target[prop] = value;
+                        acao(target);    
+                    }
+
+                    return Reflect.set(target, prop, value, receiver);
+                }
         });
     }
 
     static _ehFuncao(func) {
-        return typeof (func) == typeof (Function);
+
+        return typeof(func) == typeof(Function);
     }
 }
