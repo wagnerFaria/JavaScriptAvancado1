bc378f66ca4b0a35e8994bd9b44247ab06ab15fc
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2019-05-09 16:20:52 -0400

    Parte-2__Final

--- aluraframe/client/js/app/helpers/DateHelper.js
+++ aluraframe/client/js/app/helpers/DateHelper.js
@@ -1,20 +1,22 @@
 class DateHelper {
-    
+
     constructor() {
-        
+
         throw new Error('Esta classe não pode ser instanciada');
     }
-    
+
     static dataParaTexto(data) {
-        return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
     }
-    
+
     static textoParaData(texto) {
-        
-        if(!/\d{4}-\d{2}-\d{2}/.test(texto)) 
-            throw new Error('Deve estar no formato aaaa-mm-dd');
-             
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+
+        // mudamos a validação para aceitar o novo formato!
+        if (!/\d{2}\/\d{2}\/\d{4}/.test(texto))
+            throw new Error('Deve estar no formato dd/mm/aaaa');
+
+        // veja que usamos no split '/' no lugar de '-'. Usamos `reverse` também para ficar ano/mes/dia.      
+        return new Date(...texto.split('/').reverse().map((item, indice) => item - indice % 2));
     }
-    
+
 }
